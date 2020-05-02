49b31cacab02cba65a290fe83091c465c5d95734
Daniel-Sanchez-111 <62917581+Daniel-Sanchez-111@users.noreply.github.com>
2020-05-01 18:08:18 -0600

    Gameplay
    Agrega 3 enemigos que se desplazan hacia abajo, tambien agrega la capacidad de disparar y la colision con las naves, ademas de la puntuacion

--- main.cpp
+++ main.cpp
@@ -2,18 +2,17 @@
 #include <windows.h>
 #include <conio.h>
 #include <iostream>
+#include <stdlib.h>
+#include <list>
+
 #define izquierda 75
 #define derecha 77
 #define arriba 72
 #define abajo 80
-#define esc 27
 #define enter 13
 using namespace std;
-/*HANDLE cons;
-cons = GetStdHandle(STD_OUTPUT_HANDLE); Solo una vez por funcion
-SetConsoleTextAttribute(cons,14);
-Con esto se modifica el color de los caracteres ponganlo antes de los caracteres con el color que quieren poner y despues del texto con el color blanco otra vez
-*/
+HANDLE cons;
+int i=0;
 //Funcion para moverse
 void gotoxy(int x, int y){
     HANDLE consola;
@@ -24,35 +23,54 @@ void gotoxy(int x, int y){
     SetConsoleCursorPosition(consola,pos);
 
 }
+//Clase para crear al alien
 
 //Clase para crear la nave
 class Nave
 {
     int x,y;
     int vidas;
+    int salud;
 public:
-    Nave(int x_, int y_,int vidas_): x(x_),y(y_),vidas(vidas_){}
+    Nave(int x_, int y_,int vidas_,int salud_): x(x_),y(y_),vidas(vidas_),salud(salud_){}
+    int X();
+    int Y();
     void crear();
     void borrador();
     void mover();
     void corazon();
+    void disminuirCorazon();
     void muerte();
+    int rVidas();
 };
+int Nave::X()
+{
+    return x;
+}
+int Nave::Y()
+{
+    return y;
+}
 
+int Nave::rVidas()
+{
+    return vidas;
+}
 
 //Crea la nave
 void Nave::crear()
 {
-    gotoxy(x,y);  printf("  %c",94);
-    gotoxy(x,y+1);printf(" %c%c%c",40,238,41);
-    gotoxy(x,y+2);printf("  %c",176);
+    gotoxy(x,y);  printf("  %c ",94);
+    gotoxy(x,y+1);printf(" %c%c%c ",40,238,41);
+    gotoxy(x,y+2);printf("  %c ",176);
 }
 //Borra el rastro de la nave
 void Nave::borrador()
 {
-    gotoxy(x,y);printf("      ");
-    gotoxy(x,y+1);printf("      ");
-    gotoxy(x,y+2);printf("      ");
+    gotoxy(x-1,y);printf("       ");
+    gotoxy(x-2,y+1);printf("        ");
+    gotoxy(x-2,y+2);printf("        ");
+    gotoxy(x-2,y+3);printf("        ");
 
 }
 void Nave::mover()
@@ -62,39 +80,44 @@ void Nave::mover()
             char tecla = getch();
             borrador();
             //Compara la variable tecla para determinar hacia donde se mueve flecha izquierda = izquierda flecha derecha =Derecha
-            if(tecla==izquierda && x>5){
+            if(tecla==izquierda && x>7){
                 x-=3;
             }
-            if(tecla==derecha && x<69){
+            if(tecla==derecha && x<70){
                 x+=3;
             }
             if(tecla=='e'){
-                vidas--;
+                salud--;
             }
             crear();
         }
 }
 void Nave::corazon()
 {
+    gotoxy(65,2);printf("Vidas: %d",vidas);
     HANDLE consola;
     consola = GetStdHandle(STD_OUTPUT_HANDLE);
-    gotoxy(10,2);printf("Vidas:");
-    gotoxy(16,2);printf("     ");
-    for(int i=0;i<vidas;i++){
+    gotoxy(7,2);printf("Salud:");
+    gotoxy(13,2);printf("     ");
+    for(int i=0;i<salud;i++){
         SetConsoleTextAttribute(consola,12);
-        gotoxy(16+i,2);printf("%c",3);
+        gotoxy(13+i,2);printf("%c",3);
     }
     SetConsoleTextAttribute(consola,15);
 
 }
+void Nave::disminuirCorazon()
+{
+    salud--;
+}
 void Nave::muerte()
 {
-    if(vidas==0){
+    if(salud==0){
         borrador();
-        gotoxy(x,y);  printf("   **  ");
-        gotoxy(x,y+1);printf("  ****  ");
-        gotoxy(x,y+2);printf("   **   ");
-        Sleep(300);
+        gotoxy(x,y);  printf("  **  ");
+        gotoxy(x,y+1);printf(" **** ");
+        gotoxy(x,y+2);printf("  **  ");
+        Sleep(200);
 
         borrador();
         gotoxy(x,y);  printf("* ** *");
@@ -102,8 +125,110 @@ void Nave::muerte()
         gotoxy(x,y+2);printf("* ** *");
         Sleep(300);
         borrador();
+        vidas--;
+        salud=3;
+        corazon();
+        crear();
+
 
     }
+}
+
+class Alien
+{
+private:
+    int x;
+    float y;
+public:
+    Alien(int x_,float y_):x(x_),y(y_){}
+    void crear();
+    void mover();
+    void colision(class Nave &N);
+    void borrar();
+    int X();
+    int Y();
+};
+int Alien::X()
+{
+    return x;
+}
+int Alien::Y()
+{
+    return y;
+}
+void Alien::crear()
+{
+    gotoxy(x,y);printf("%c%c%c%c",60,40,41,62);
+}
+void Alien::mover()
+{
+    gotoxy(x,y);printf("     ");
+    y+=0.7;
+    if(y>32)
+    {
+        x = rand()%66+4;
+        y=4;
+    }
+    crear();
+}
+void Alien::borrar()
+{
+    gotoxy(x,y);printf("    ");
+}
+void Alien::colision(class Nave &N)
+{
+    if(x>=N.X()-3&& x<=N.X()+5 && y>=N.Y()&& y<=N.Y()+3)
+    {
+        N.disminuirCorazon();
+        borrar();
+        N.crear();
+        N.corazon();
+        x = rand()%68+5;
+        y=4;
+    }
+}
+
+class Disparo{
+    int x,y;
+public:
+    Disparo(int x_,int y_):x(x_),y(y_){}
+    void mover();
+    bool eliminar();
+    int X();
+    int Y();
+};
+void Disparo::mover()
+{
+
+    cons = GetStdHandle(STD_OUTPUT_HANDLE);
+    gotoxy(x,y);printf(" ");
+    SetConsoleTextAttribute(cons,i);
+    y--;
+    gotoxy(x,y);printf("%c",221);
+    if(i==15)
+    {
+        i=0;
+    }
+    i++;
+    SetConsoleTextAttribute(cons,15);
+}
+bool Disparo::eliminar()
+{
+   if(y==4)
+   {
+        return true;
+   }else
+   {
+       return false;
+   }
+}
+int Disparo::X()
+{
+    return x;
+}
+int Disparo::Y()
+{
+    return y;
 }
 
 //Funcion para ocultar el parpadeo del cursor
@@ -121,7 +246,7 @@ void estrellas() {
 
     SetConsoleTextAttribute(hConsole, 15);
     gotoxy(42, 7); cout << "+";  gotoxy(13, 22); cout << "+";
-    gotoxy(64, 30); cout << "+";  gotoxy(45, 14); cout << "+";
+    gotoxy(64, 30); cout << "+";  gotoxy(39, 14); cout << "+";
     gotoxy(22, 27); cout << "+";  gotoxy(58, 24); cout << "+";
     gotoxy(8, 8); cout << "+";  gotoxy(58, 10); cout << "+";
     gotoxy(26, 15); cout << "+";  gotoxy(15, 13); cout << "+";
@@ -216,19 +341,21 @@ void creditos()
     cons = GetStdHandle(STD_OUTPUT_HANDLE);
     limpiarPantalla();
     SetConsoleTextAttribute(cons,14);
-    gotoxy(38,1);printf("Creditos\n");
+    gotoxy(23,1);printf("  ____              _ _ _            ");
+    gotoxy(23,2);printf(" / ___|_ __ ___  __| (_) |_ ___  ___ ");
+    gotoxy(23,3);printf("| |   | '__/ _ \\/ _` | | __/ _ \\/ __|");
+    gotoxy(23,4);printf("| |___| | |  __/ (_| | | || (_) \\__ \\");
+    gotoxy(23,5);printf(" \\____|_|  \\___|\\__,_|_|\\__\\___/|___/");
     SetConsoleTextAttribute(cons,7);
-    gotoxy(20,5);printf("Universidad Autonoma de Baja California Sur\n");
-    gotoxy(17,7);printf("Departamento Academico de Sistemas Computacionales\n");
-    gotoxy(35,10);printf("Moon Cresta\n");
-    gotoxy(27,13);printf("Integrantes:\n");
-    gotoxy(27,15);printf("-Cota Verdugo Isael\n");
-    gotoxy(27,17);printf("-Juarez Sotelo Jesus Xavier\n");
-    gotoxy(27,19);printf("-Sanchez Carrillo Daniel Eduardo\n");
-    gotoxy(34,22);printf("Programacion I\n");
-    gotoxy(29,25);printf("Prof. Jonathan Soto Munoz\n");
-    gotoxy(20,28);printf("Presione ENTER para volver al menu principal\n");
-    gotoxy(26,30);printf("Presione otra tecla para salir\n");
+    gotoxy(20,13);printf("Universidad Autonoma de Baja California Sur");
+    gotoxy(17,15);printf("Departamento Academico de Sistemas Computacionales");
+    gotoxy(35,18);printf("Moon Cresta");
+    gotoxy(27,21);printf("Integrantes:");
+    gotoxy(27,23);printf("-Cota Verdugo Isael");
+    gotoxy(27,25);printf("-Juarez Sotelo Jesus Xavier");
+    gotoxy(27,27);printf("-Sanchez Carrillo Daniel Eduardo");
+    gotoxy(34,29);printf("Programacion I");
+    gotoxy(29,32);printf("Prof. Jonathan Soto Munoz");
     estrellas();
 }
 
@@ -249,74 +376,169 @@ void limites()
     gotoxy(77,33);printf("%c",188);
 }
 
+void score(int puntos)
+{
+    cons = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(cons,14);
+    gotoxy(33,2);printf("Puntuacion: ");
+    SetConsoleTextAttribute(cons,15);
+    printf("%d",puntos);
+}
+void final(int puntos)
+{
+    system("cls");
+    gotoxy(35,15);printf("PUNTUACION: %d",puntos);
+    gotoxy(20,28);printf("Presione ENTER para volver al menu principal");
+}
+
 void cancionMenu()
 {
     PlaySound(TEXT("menu.wav"),NULL,SND_ASYNC);
 }
+
 void cancionCred()
 {
-    PlaySound(TEXT("cred.wav"),NULL,SND_ASYNC);
+    PlaySound(TEXT("CaI.wav"),NULL,SND_ASYNC);
 }
 void cancionJuego()
 {
     PlaySound(TEXT("juego.wav"),NULL,SND_ASYNC);
 }
-
+void cancionInstrucciones()
+{
+    PlaySound(TEXT("CaI.wav"),NULL,SND_ASYNC);
+}
 void instrucciones()
 {
 
 
-    HANDLE cons;
+
     cons = GetStdHandle(STD_OUTPUT_HANDLE);
     limpiarPantalla();
     SetConsoleTextAttribute(cons,14);
-    gotoxy(35,1);printf("Instrucciones\n");
+     gotoxy(1,1);printf(" _____              _");
+    gotoxy(1,2);printf("|_   _|            | |                           (_)");
+    gotoxy(1,3);printf("  | |   _ __   ___ | |_  _ __  _   _   ___   ___  _   ___   _ __    ___  ___ ");
+    gotoxy(1,4);printf("  | |  |  _ \\ / __|| __||  __|| | | | / __| / __|| | / _ \\ |  _ \\  / _ \\/ __|");
+    gotoxy(1,5);printf(" _| |_ | | | |\\__ \\| |_ | |   | |_| || (__ | (__ | || (_) || | | ||  __/\\__ \\");
+    gotoxy(1,6);printf(" \\___/ |_| |_||___/ \\__||_|    \\__,_| \\___| \\___||_| \\___/ |_| |_| \\___||___/");
     SetConsoleTextAttribute(cons,7);
-    gotoxy(8,5);printf("El objetivo principal de Moon Cresta es destruir a los alienigenas.\n");
-    gotoxy(27,10);printf("Controles\n");
-    gotoxy(27,12);printf("<- Moverse a la izquierda\n");
-    gotoxy(27,14);printf("-> Moverse a la derecha\n");
-    gotoxy(27,16);printf("[A] Disparar\n");
-    gotoxy(27,18);printf("[ESC] Menu de pausa\n");
-    gotoxy(20,28);printf("Presione ENTER para volver al menu principal\n");
-    gotoxy(26,30);printf("Presione otra tecla para salir\n");
+    gotoxy(8,10);printf("El objetivo principal de Moon Cresta es destruir a los alienigenas.");
+    gotoxy(27,14);printf("Controles");
+    gotoxy(27,16);printf("<- Moverse a la izquierda");
+    gotoxy(27,18);printf("-> Moverse a la derecha");
+    gotoxy(27,20);printf("[A] Disparar");
+    gotoxy(27,22);printf("[ESC] Menu de pausa");
+    gotoxy(20,28);printf("Presione ENTER para volver al menu principal");
+    gotoxy(26,30);printf("Presione otra tecla para salir");
     estrellas();
 }
 
 int main()
 {
-    //Crear el objeto
-    Nave ob(38,27,3);
-    cursor_ocultar();
+    SetConsoleTitleA("Moon Cresta");
     const char *titulo = "Moon Cresta";
     const char *opciones[] = {"Jugar","Instrucciones","Creditos","Salir"};
+
+    cursor_ocultar();
+    int puntos=0;
     int n = 4;
     int tecla;
     int opcion;
     bool fin = false;
     bool men = true;
+    list<Disparo*> D;
+    list<Disparo*>:: iterator i_Disparo;
+    list<Alien*>A;
+    list<Alien*>::iterator i_Alien;
+    for(int i=0;i<3;i++)
+    {
+        //Crea los objetos alien en una lista
+        A.push_back(new Alien(rand()%66+4,rand()%5+4));
+    }
     while(men)
     {
-    cancionMenu();
+    //cancionMenu();
     opcion = menu(titulo,opciones,n);
     limpiarPantalla();
     PlaySound(NULL,NULL,0);
+    Nave ob(38,27,3,3);
+    Alien al(10,4);
     switch(opcion){
+
         case 1:
+            //Crear el objeto
+            puntos=0;
+            fin=false;
             limites();
-            cancionJuego();
+            //cancionJuego();
             //Llamar a los metodos de la clase
             ob.crear();
             //Hace que el juego se siga ejecutando
             while(!fin){
+                if(kbhit())
+                {
+                //Crea el disparo
+                if(GetAsyncKeyState(0x20)){
+                    D.push_back(new Disparo(ob.X()+2, ob.Y()-1));
+                }
+
+                }
+                for(i_Disparo=D.begin();i_Disparo!=D.end();i_Disparo++)
+                {
+                    //Controla el disparo y  lo elimina si y es igual a 4
+                    (*i_Disparo)->mover();
+                    if((*i_Disparo)->eliminar())
+                    {
+                        gotoxy((*i_Disparo)->X(),(*i_Disparo)->Y());printf(" ");
+                        delete(*i_Disparo);
+                        i_Disparo=D.erase(i_Disparo);
+                    }
+                }
+                //Controla el objeto alien
+                for(i_Alien=A.begin();i_Alien!=A.end();i_Alien++)
+                {
+                    (*i_Alien)->mover();
+                    (*i_Alien)->colision(ob);
+                }
+                for(i_Alien=A.begin();i_Alien!=A.end();i_Alien++)
+                {
+                    for(i_Disparo=D.begin();i_Disparo!=D.end();i_Disparo++)
+                    {
+                        if((*i_Alien)->X()==(*i_Disparo)->X()||((*i_Alien)->X()+1==(*i_Disparo)->X()||(*i_Alien)->X()+2==(*i_Disparo)->X()||(*i_Alien)->X()+3==(*i_Disparo)->X())&&((*i_Alien)->Y()+1==(*i_Disparo)->Y()|| (*i_Alien)->Y()==(*i_Disparo)->Y()))
+                           {
+                               gotoxy((*i_Disparo)->X(),(*i_Disparo)->Y());printf(" ");
+                               delete(*i_Disparo);
+                               i_Disparo=D.erase(i_Disparo);
+
+                               A.push_back(new Alien(rand()%66+4,4));
+                               gotoxy((*i_Alien)->X(),(*i_Alien)->Y());printf("    ");
+                               delete(*i_Alien);
+                               i_Alien=A.erase(i_Alien);
+                               puntos+=10;
+                           }
+                    }
+                }
                 ob.corazon();
                 ob.mover();
                 ob.muerte();
                 Sleep(30);
-
+                score(puntos);
+                if(ob.rVidas()==0)
+                {
+                    fin=true;
+                    final(puntos);
+                    tecla=getch();
+                    if(tecla==enter)
+                    {
+                        break;
+                    }
+                }
             }
+            break;
         case 2:
             instrucciones();
+            //cancionInstrucciones();
             tecla=getch();
             if(tecla==enter)
             {
@@ -325,9 +547,10 @@ int main()
             {
                 return 0;
             }
+            break;
         case 3:
             creditos();
-            cancionJuego();
+            //cancionCred();
             tecla=getch();
             if(tecla==enter)
             {
@@ -336,6 +559,7 @@ int main()
             {
                 return 0;
             }
+            break;
         case 4:
             return 0;
             break;
